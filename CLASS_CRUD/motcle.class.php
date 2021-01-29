4502540bd63f966cbf6d37973aaaf5ea4002994c
MisterAzix <flizi@live.fr>
2021-01-29 12:48:32 +0100

    Adding MOTCLE & THEMATIQUE classes

---
+++ CLASS_CRUD/motcle.class.php
@@ -0,0 +1,96 @@
+<?php
+// CRUD MOTCLE (ETUD)
+
+require_once __DIR__ . '../../CONNECT/database.php';
+
+class MOTCLE
+{
+	function get_1Motcle($numMotCle)
+	{
+		global $db;
+		$query = $db->prepare("SELECT * FROM motcle WHERE numMotCle=:numMotCle");
+		$query->execute([
+			'numMotCle' => $numMotCle
+		]);
+		$result = $query->fetch(PDO::FETCH_OBJ);
+		return $result;
+	}
+
+	function get_AllMotscles()
+	{
+		global $db;
+		$query = $db->query('SELECT * FROM motcle');
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return $result;
+	}
+
+	function get_AllMotClesByLang($numLang)
+	{
+		global $db;
+		$query = $db->prepare('SELECT * FROM motcle WHERE numLang = :numLang');
+		$query->execute([
+			'numLang' => $numLang
+		]);
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return ($result);
+	}
+
+	function create($numMotCle, $libMotCle, $numLang)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('INSERT INTO motcle (numMotCle, libMotCle, numLang) VALUES (:numMotCle, :libMotCle, :numLang)');
+			$query->execute([
+				'numMotCle' => $numMotCle,
+				'libMotCle' => $libMotCle,
+				'numLang' => $numLang
+			]);
+			$db->commit();
+			$query->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur insert MOTCLE : ' . $e->getMessage());
+		}
+	}
+
+	function update($numMotCle, $libMotCle, $numLang)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('UPDATE motcle SET libMotCle = :libMotCle, numLang = :numLang WHERE numMotCle = :numMotCle');
+			$query->execute([
+				'numMotCle' => $numMotCle,
+				'libMotCle' => $libMotCle,
+				'numLang' => $numLang
+			]);
+			$db->commit();
+			$query->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur update MOTCLE : ' . $e->getMessage());
+		}
+	}
+
+	function delete($numMotCle)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('DELETE FROM motcle WHERE numMotCle=:numMotCle');
+			$query->execute([
+				'numMotCle' => $numMotCle
+			]);
+			$db->commit();
+			$query->closeCursor();
+			return $query->rowCount();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur delete MOTCLE : ' . $e->getMessage());
+		}
+	}
+}	// End of class
---
+++ CLASS_CRUD/thematique.class.php
@@ -0,0 +1,95 @@
+<?php
+// CRUD THEMATIQUE (ETUD)
+
+require_once __DIR__ . '../../CONNECT/database.php';
+
+class THEMATIQUE
+{
+	function get_1Thematique($numThem)
+	{
+		global $db;
+		$query = $db->prepare("SELECT * FROM thematique WHERE numThem=:numThem");
+		$query->execute([
+			'numThem' => $numThem
+		]);
+		$result = $query->fetch(PDO::FETCH_OBJ);
+		return $result;
+	}
+
+	function get_AllThematiques()
+	{
+		global $db;
+		$query = $db->query('SELECT * FROM thematique');
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return $result;
+	}
+
+	function get_AllThematiquesByLang($numLang){
+		global $db;
+		$query = $db->prepare('SELECT * FROM thematique WHERE numLang = :numLang');
+		$query->execute([
+			'numLang' => $numLang
+		]);
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return ($result);
+	}
+
+	function create($numThem, $libThem, $numLang)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('INSERT INTO thematique (numThem, libThem, numLang) VALUES (:numThem, :libThem, :numLang)');
+			$query->execute([
+				'numThem' => $numThem,
+				'libThem' => $libThem,
+				'numLang' => $numLang
+			]);
+			$db->commit();
+			$query->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur insert THEMATIQUE : ' . $e->getMessage());
+		}
+	}
+
+	function update($numThem, $libThem, $numLang)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('UPDATE thematique SET libThem = :libThem, numLang = :numLang WHERE numThem = :numThem');
+			$query->execute([
+				'numThem' => $numThem,
+				'libThem' => $libThem,
+				'numLang' => $numLang
+			]);
+			$db->commit();
+			$query->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur update THEMATIQUE : ' . $e->getMessage());
+		}
+	}
+
+	function delete($numThem)
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('DELETE FROM thematique WHERE numThem=:numThem');
+			$query->execute([
+				'numThem' => $numThem
+			]);
+			$db->commit();
+			$query->closeCursor();
+			return $query->rowCount();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur delete THEMATIQUE : ' . $e->getMessage());
+		}
+	}
+}	// End of class
