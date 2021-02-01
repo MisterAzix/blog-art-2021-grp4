aa5cabf1d9f18b989bd9fddf40c526c84fc95331
MisterAzix <flizi@live.fr>
2021-02-01 04:27:33 +0100

    CRUD ARTICLE : basic display

--- back/article/article.php
+++ back/article/article.php
@@ -10,9 +10,17 @@
 // Mode DEV
 require_once __DIR__ . '/../../util/utilErrOn.php';
 
+// Insertion classe ARTICLE
+require_once __DIR__ . '/../../CLASS_CRUD/article.class.php';
+$article = new ARTICLE();
+
+// Appel méthode : tous les angles en BDD
+$all = $article->get_AllArticles();
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <title>Admin - Gestion du CRUD Article</title>
@@ -20,19 +28,49 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>BLOGART21 Admin - Gestion du CRUD Article</h1>
+    <main class="container">
+        <div class="d-flex flex-column">
+            <h1>BLOGART21 Admin - Gestion du CRUD Article</h1>
+            <hr>
+            <h2>Nouvel article : <a href="./createArticle.php"><i>Create an article</i></a></h2>
 
-    <br><br>
+            <?php foreach ($all as $row) : ?>
 
-    <h2>En construction :-)</h2>
+                <hr>
 
-    <br><br>
+                <div class="row">
+                    <div class="col-8">
+                        <h2><?= $row->libTitrArt ?> (<?= $row->numAngl ?> - <?= $row->numThem ?>)</h2>
+                        <h5><?= $row->libChapoArt ?></h5>
+                        <p><?= $row->libAccrochArt ?></p>
 
-<?php
-require_once __DIR__ . '/footer.php';
-?>
+                        <p><?= $row->parag1Art ?></p>
+
+                        <h6><?= $row->libSsTitr1Art ?></h6>
+                        <p><?= $row->parag2Art ?></p>
+
+                        <h6><?= $row->libSsTitr2Art ?></h6>
+                        <p><?= $row->parag3Art ?></p>
+
+                        <p><?= $row->libConclArt ?></p>
+
+                        <a href="./updateAngle.php?id=<?= $row->numArt ?>"><i>Modifier</i></a>
+                        <a href="./deleteAngle.php?id=<?= $row->numArt ?>"><i>Supprimer</i></a>
+                    </div>
+                    <div class="col-4">
+                        <img src="../../upload/<?= $row->urlPhotArt ?>" alt="">
+                    </div>
+                </div>
+
+            <?php endforeach ?>
+
+            <?php require_once __DIR__ . '/footer.php' ?>
+        </div>
+    </main>
 </body>
+
 </html>
