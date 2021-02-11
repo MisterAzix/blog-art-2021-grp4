70e4063c19cab6271bb180f9ba78139beb999c89
MisterAzix <flizi@live.fr>
2021-02-11 10:40:03 +0100

    CRUD USER : display

--- back/user/user.php
+++ back/user/user.php
@@ -1,7 +1,7 @@
 <?php
 /////////////////////////////////////////////////////
 //
-//  CRUD USER (PDO) - Modifié - 6 Décembre 2020
+//  CRUD USER (PDO) - Modifié - 11 Février 2021
 //
 //  Script  : user.php  (ETUD)   -   BLOGART21
 //
@@ -10,9 +10,19 @@
 // Mode DEV
 require_once __DIR__ . '/../../util/utilErrOn.php';
 
+// Insertion classe USER
+require_once __DIR__ . '/../../CLASS_CRUD/user.class.php';
+require_once __DIR__ . '/../../CLASS_CRUD/statut.class.php';
+$user = new USER();
+$statut = new STATUT();
+
+// Appel méthode : tous les users en BDD
+$all = $user->get_AllUsers();
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <title>Admin - Gestion du CRUD User</title>
@@ -20,19 +30,53 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>BLOGART21 Admin - Gestion du CRUD User</h1>
+    <main class="container">
+        <div class="d-flex flex-column">
+            <h1>BLOGART21 Admin - Gestion du CRUD User</h1>
+            <hr>
+            <h2>Nouveau user : <a href="./createUser.php"><i>Create a user</i></a></h2>
+            <hr>
+            <h2>Tous les user</h2>
 
-    <br><br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Pseudo</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Email</th>
+                        <th>Statut</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($all as $row) : ?>
+                        <tr>
+                            <td>
+                                <h4> <?= $row->pseudoUser ?> </h4>
+                            </td>
+                            <td> <?= $row->nomUser ?> </td>
+                            <td> <?= $row->prenomMemb ?> </td>
+                            <td> <?= $row->eMailUser ?> </td>
+                            <td> <?= $statut->get_1Statut($row->idStat)->libStat ?> </td>
+                            <td><a href="./updateUser.php?id=<?= $row->pseudoUser ?>"><i>Modifier</i></a>
+                                <br>
+                            </td>
+                            <td><a href="./deleteUser.php?id=<?= $row->pseudoUser ?>"><i>Supprimer</i></a>
+                                <br>
+                            </td>
+                        </tr>
+                    <?php endforeach  ?>
+                </tbody>
+            </table>
 
-    <h2>En construction :-)</h2>
-
-    <br><br>
-
-<?php
-require_once __DIR__ . '/footer.php';
-?>
+            <?php require_once __DIR__ . '/footer.php' ?>
+        </div>
+    </main>
 </body>
+
 </html>
