1353d7b00b424a1b3fc02dfa10e209405dd1c755
MisterAzix <flizi@live.fr>
2021-02-11 16:29:26 +0100

    Adding a working file to see component style

---
+++ front/includes/work.php
@@ -0,0 +1,16 @@
+<?php
+//========================================//
+//
+//             work.php (temp)
+//
+//========================================//
+
+require_once '../../commons/header.php';
+
+// PUT YOUR COMPONENT REQUIRE BELOW
+// ex: require './components/button.php';
+
+
+
+require_once '../../commons/fooer.php'
+?>
