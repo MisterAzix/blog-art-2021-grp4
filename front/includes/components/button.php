b32130b6c02ed6868250fe6df78d200cfea70978
Adel Sanaa <adel.sanaa@mmibordeaux.com>
2021-02-12 11:47:18 +0100

    buttons components

--- front/includes/components/button.php
+++ front/includes/components/button.php
@@ -1,3 +1,26 @@
-<button>
-
+<button id="basebutton" href="../register/index.php">
+    s'inscrire
 </button>
+<br>
+<br>
+<button class ="connexion" href="../login/index.php">
+    se connecter
+</button>
+<br>
+<br>
+<button class="lire" href="../article/index.php">
+    Lire l'article
+</button>
+
+<br>
+<br>
+
+<div>
+  <input type="checkbox" id="accepter" name="accepter">
+  <label for="accepter">J'accepte les <a href="../cgu/index.php">conditions générales d'utilisations</a>.</label>
+</div>
+
+<div>
+  <input type="checkbox" id="newsletter" name="newsletter">
+  <label for="newsletter">J'adhère à la newsletter.</label>
+</div>
