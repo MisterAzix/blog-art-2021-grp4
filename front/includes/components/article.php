81b0bec8374be66703f4ccc6c7d39b66a62c4e98
SebastienBn <seb.aguilar@homtail.com>
2021-02-13 12:39:09 +0100

    article finished

--- front/includes/components/article.php
+++ front/includes/components/article.php
@@ -1,3 +1,31 @@
 <article>
-
+    <div class="left-part">
+        <p class="infos">
+            <span>Évenements</span>
+            | Publié le 09 Février 2021 à 10h35
+        </p>
+        <h2>
+        Bordeaux, écologie et environnement
+        </h2>
+        <p class="article-info">
+        Les projets ambitieux de la Mairie de Bordeaux ne concordent pas 
+        toujours avec l’opinion publique. Il fait chaud, très chaud même, et les solutions 
+        à prendre ne sont pas toujours faciles. Surtout en ce moment d’urgence climatique !
+        </p>
+        <?php 
+            $buttonTitle = "Lire l'article";
+            $buttonHref = '';
+            require '../../components/button.php';
+        ?>
+    </div>
+    <img src="../../../assets/images/drone.jpg" alt="photo colorée de bordeaux vue de haut">
+    <div class="top-left-corner">
+    </div>
 </article>
+
+
+<!--    
+        <p class="button">
+        Lire l'article
+        </p>
+ -->
