43424f510b77487267725e311863c591e8750f84
EliiiseE <elise.echasseriau@mmibordeaux.com>
2021-02-15 17:02:11 +0100

    article v1

--- front/includes/pages/plan/index.php
+++ front/includes/pages/plan/index.php
@@ -15,15 +15,18 @@ require_once '../../commons/header.php';
     </div>
     <div class="sections">
         <div class="section_container">
-            <h2>Acces rapide</h2>
-            <p>Inscription</p>
-            <p>Connexion</p>
-            <p>Me contacter</p>
+            <div class="section">
+                <h2>Acces rapide</h2>
+                <p>Inscription</p>
+                <p>Connexion</p>
+                <p>Me contacter</p>
+            </div>
         </div>
             <svg width="4" height="102" viewBox="0 0 4 102" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 0L2 102" stroke="#BFF088" stroke-width="4"/>
             </svg>
         <div class="section_container">
+        <div class="section">
             <h2>Rubriques</h2>
             <p>Actualité</p>
             <p>Ecologie</p>
@@ -31,14 +34,18 @@ require_once '../../commons/header.php';
             <p>Evenements</p>
             <p>Culture</p>
         </div>
+
+        </div>
             <svg width="4" height="102" viewBox="0 0 4 102" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 0L2 102" stroke="#BFF088" stroke-width="4"/>
             </svg>
         <div class="section_container">
-            <h2>tous mes articles</h2>
-            <p>Bordeaux, écologie et environnement</p>
-            <p>Écologique et insolite,c’est possible !</p>
-            <p>Phillipe Barre : Anticonformiste et créateur d’un éco-systeme</p>
+            <div class="section">
+                <h2>tous mes articles</h2>
+                <p>Bordeaux, écologie et environnement</p>
+                <p>Écologique et insolite,c’est possible !</p>
+                <p>Phillipe Barre : Anticonformiste et créateur d’un éco-systeme</p>
+            </div>
         </div>
     </div>
 </div>
