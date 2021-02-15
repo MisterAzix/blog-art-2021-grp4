51916dfdf3fb073d61623fffe5eb40f218dfb4e7
MisterAzix <flizi@live.fr>
2021-02-15 02:54:48 +0100

    CRUD LIKECOM : display

--- CLASS_CRUD/likecom.class.php
+++ CLASS_CRUD/likecom.class.php
@@ -5,23 +5,33 @@ require_once __DIR__ . '../../CONNECT/database.php';
 
 class LIKECOM
 {
-	function get_1LikeCom($numArt, $numMemb)
+	function get_1LikeCom($numMemb, $numSeqCom, $numArt)
 	{
 		global $db;
-		$query = $db->prepare("SELECT * FROM likecom WHERE numArt=:numArt AND numMemb=:numMemb");
+		$query = $db->prepare("SELECT * FROM likecom WHERE numMemb=:numMemb AND numSeqCom=:numSeqCom AND numArt=:numArt");
 		$query->execute([
-			'numAngl' => $numArt,
-			'numAngl' => $numMemb
+			'numMemb' => $numMemb,
+			'numSeqCom' => $numSeqCom,
+			'numArt' => $numArt
 		]);
 		$result = $query->fetch(PDO::FETCH_OBJ);
 		return $result;
 	}
 
-	function get_AllLikesCom($numArt)
+	function get_AllLikesCom()
 	{
 		global $db;
-		$query = $db->prepare('SELECT * FROM likecom WHERE numArt = :numArt');
+		$query = $db->query('SELECT * FROM likecom');
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return $result;
+	}
+
+	function get_AllLikesComByComment($numSeqCom, $numArt)
+	{
+		global $db;
+		$query = $db->prepare('SELECT * FROM likecom WHERE numSeqCom = :numSeqCom AND numArt = :numArt');
 		$query->execute([
+			'numSeqCom' => $numSeqCom,
 			'numArt' => $numArt
 		]);
 		$result = $query->fetchAll(PDO::FETCH_OBJ);
@@ -39,63 +49,23 @@ class LIKECOM
 		return ($result);
 	}
 
-	/*function create($libAngl, $numLang)
+	function createOrUpdate($numMemb, $numSeqCom, $numArt)
 	{
 		global $db;
-		require_once __DIR__ . '/getNextNumAngl.php';
-		$numAngl = getNextNumAngl($numLang);
 		try {
 			$db->beginTransaction();
-			$query = $db->prepare('INSERT INTO likecom (numAngl, libAngl, numLang) VALUES (:numAngl, :libAngl, :numLang)');
+			$query = $db->prepare('INSERT INTO likecom (numMemb, numSeqCom, numArt, likeC) VALUES (:numMemb, :numSeqCom, :numArt, 1) ON DUPLICATE KEY UPDATE likeC = !likeC');
 			$query->execute([
-				'numAngl' => $numAngl,
-				'libAngl' => $libAngl,
-				'numLang' => $numLang
+				'numMemb' => $numMemb,
+				'numSeqCom' => $numSeqCom,
+				'numArt' => $numArt
 			]);
 			$db->commit();
 			$query->closeCursor();
 		} catch (PDOException $e) {
 			$db->rollBack();
 			$query->closeCursor();
-			die('Erreur insert LIKECOM : ' . $e->getMessage());
+			die('Erreur insertOrUpdate LIKECOM : ' . $e->getMessage());
 		}
 	}
-
-	function update($numAngl, $libAngl)
-	{
-		global $db;
-		try {
-			$db->beginTransaction();
-			$query = $db->prepare('UPDATE likecom SET libAngl = :libAngl WHERE numAngl = :numAngl');
-			$query->execute([
-				'numAngl' => $numAngl,
-				'libAngl' => $libAngl
-			]);
-			$db->commit();
-			$query->closeCursor();
-		} catch (PDOException $e) {
-			$db->rollBack();
-			$query->closeCursor();
-			die('Erreur update LIKECOM : ' . $e->getMessage());
-		}
-	}
-
-	function delete($numAngl)
-	{
-		global $db;
-		try {
-			$db->beginTransaction();
-			$query = $db->prepare('DELETE FROM likecom WHERE numAngl=:numAngl');
-			$query->execute([
-				'numAngl' => $numAngl
-			]);
-			$db->commit();
-			$query->closeCursor();
-			return $query->rowCount();
-		} catch (PDOException $e) {
-			$db->rollBack();
-			$query->closeCursor();
-			die('Erreur delete LIKECOM : ' . $e->getMessage());
-		}
-	}*/
 }	// End of class
