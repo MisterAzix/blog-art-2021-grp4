ec7d0f48f68ad7576ff8dc718058156c09bf7d07
MisterAzix <flizi@live.fr>
2021-02-25 19:43:15 +0100

    Back style beginning

--- back/common/header.php
+++ back/common/header.php
@@ -26,6 +26,7 @@ if (!$auth->is_admin()) header('Location: /accueil');
     <meta name="robots" content="noindex">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/back/css/style.css">
 </head>
 
 <body>
