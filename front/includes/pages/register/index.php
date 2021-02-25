f640689eb0bd3504e5f1e2ba896972c545fa6d63
MisterAzix <flizi@live.fr>
2021-02-25 10:50:01 +0100

    Fix style on login & register

--- front/includes/pages/register/index.php
+++ front/includes/pages/register/index.php
@@ -22,32 +22,32 @@ require_once '../../commons/header.php';
         <h2>Hey ! Inscris-toi ici ! </h2>
         <div class="input_container">
             <form action="" method="POST">
-                <div class="group">
+                <div class="input-group">
                     <input class="input" name="firstname" type="text" placeholder="Renseigne ton prénom" required />
                     <label>Prénom</label>
                 </div>
-                <div class="group">
+                <div class="input-group">
                     <input class="input" name="lastname" type="text" placeholder="Renseigne ton nom" required />
                     <label>Nom</label>
                 </div>
-                <div class="group">
+                <div class="input-group">
                     <input class="input" name="pseudo" type="text" placeholder="Renseigne ton pseudo" required />
                     <label>Pseudo *</label>
                 </div>
-                <div class="group">
+                <div class="input-group">
                     <input class="input" name="etext" type="text" placeholder="Renseigne ton email" required />
                     <label>Email *</label>
                 </div>
-                <div class="group">
+                <div class="input-group">
                     <input class="input" name="password" type="text" placeholder="Renseigne ton mot de passe" required />
                     <label>Mot de passe *</label>
-                    <p class="tips">Le mot de passe doit contenir entre 8 et 64 caractères, des lettres minuscules et majuscules et au moins un caractère spécial.</p>
+                    <span class="tips">Le mot de passe doit contenir entre 8 et 64 caractères, des lettres minuscules et majuscules et au moins un caractère spécial.</span>
                 </div>
 
                 <div class="checkbox-container">
                     <div class="checkbox">
-                        <input type="checkbox" id="accepter" name="accepter">
-                        <label for="accepter">J'accepte les <a href="../cgu/index.php">conditions générales d'utilisations</a>.</label>
+                        <input type="checkbox" id="cgu" name="cgu">
+                        <label for="cgu">J'accepte les <a href="../cgu/index.php">conditions générales d'utilisations</a>.</label>
                     </div>
 
                     <div class="checkbox">
@@ -55,8 +55,9 @@ require_once '../../commons/header.php';
                         <label for="newsletter">J'adhère à la newsletter.</label>
                     </div>
                 </div>
-
-                <button class="button">S'inscrire</button>
+                <div class="input-group">
+                    <button class="button button-submit">S'inscrire</button>
+                </div>
             </form>
         </div>
     </div>
