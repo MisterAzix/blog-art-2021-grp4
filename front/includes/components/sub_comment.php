8e2bb79f434c08d99e4c0f9968f6b86e2c491b85
SebastienBn <seb.aguilar@homtail.com>
2021-02-25 14:43:58 +0100

    comments components

---
+++ front/includes/components/sub_comment.php
@@ -0,0 +1,10 @@
+<section class="sub_comment_container">
+    <div class="user_info">
+        <h3>Clement renou</h3>
+        <p>il y a 7 min</p>
+    </div>
+
+    <div class="comment">
+        <p>Cet article est rtrès bien rédigé, je peux cependant  noter qu’à 8:34 il y a une faute d’ortographe. En effet, le COD étant placé avant le verbe et étant utilisé avec l’auxiliaire avoir, il se doit d’être accordé.  </p>
+    </div>
+</section>
