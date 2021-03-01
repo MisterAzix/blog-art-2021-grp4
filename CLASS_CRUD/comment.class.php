2d1449ac1b8cea2cb5a6619d99f36bbd8ca4c9b6
MisterAzix <flizi@live.fr>
2021-03-01 01:25:43 +0100

    CRUD Comment : CLASS

--- CLASS_CRUD/comment.class.php
+++ CLASS_CRUD/comment.class.php
@@ -4,8 +4,16 @@
 require_once __DIR__ . '../../CONNECT/database.php';
 
 class COMMENT
-{
-	function get_1Comment($numSeqCom, $numArt)
+{	
+	/**
+	 * get_1Comment Permet de récupérer un seul commentaire en base de donnée
+	 *
+	 * @param  mixed $numSeqCom
+	 * @param  mixed $numArt
+	 * @return object Renvoie un object comprenant les informations du commentaire récupéré
+	 * @return bool false si rien n'est trouvé en base de donnée
+	 */
+	function get_1Comment(int $numSeqCom, int $numArt)
 	{
 		global $db;
 		$query = $db->prepare("SELECT * FROM comment WHERE numSeqCom=:numSeqCom AND numArt = :numArt");
@@ -16,7 +24,13 @@ class COMMENT
 		$result = $query->fetch(PDO::FETCH_OBJ);
 		return $result;
 	}
-
+	
+	/**
+	 * get_AllComments Permet de récupérer tous les commentaires en base de donnée
+	 *
+	 * @return array Renvoie un tableau d'object avec les informations de chaque commentaire
+	 * @return bool false si rien n'est trouvé en base de donnée
+	 */
 	function get_AllComments()
 	{
 		global $db;
@@ -24,7 +38,14 @@ class COMMENT
 		$result = $query->fetchAll(PDO::FETCH_OBJ);
 		return $result;
 	}
-
+	
+	/**
+	 * get_AllCommentsByArticle Permet de récupérer tous les commentaires en base de donnée en fonction d'un article
+	 *
+	 * @param  mixed $numArt
+	 * @return array Renvoie un tableau d'object avec les informations des commentaires récupérés
+	 * @return bool false si rien n'est trouvé en base de donnée
+	 */
 	function get_AllCommentsByArticle($numArt)
 	{
 		global $db;
@@ -35,8 +56,33 @@ class COMMENT
 		$result = $query->fetchAll(PDO::FETCH_OBJ);
 		return $result;
 	}
-
-	function get_AllCommentsByMembre($numMemb)
+	
+	/**
+	 * get_AllowedMainCommentsByArticle Permet de récupérer tous les commentaires non logiquement supprimé d'un article
+	 *
+	 * @param  mixed $numArt
+	 * @return array Renvoie un tableau d'object avec les informations des commentaires récupérés
+	 * @return bool false si rien n'est trouvé en base de donnée
+	 */
+	function get_AllowedMainCommentsByArticle(int $numArt)
+	{
+		global $db;
+		$query = $db->prepare("SELECT * FROM comment WHERE numArt = :numArt AND numSeqCom NOT IN (SELECT numSeqComR FROM commentplus WHERE numArt = :numArt) AND attModOK = 1 AND affComOK = 1 AND delLogiq = 0");
+		$query->execute([
+			'numArt' => $numArt
+		]);
+		$result = $query->fetchAll(PDO::FETCH_OBJ);
+		return $result;
+	}
+	
+	/**
+	 * get_AllCommentsByMembre Permet de récupérer tous les commentaires d'un membre
+	 *
+	 * @param  mixed $numMemb
+	 * @return array Renvoie un tableau d'object avec les informations des commentaires récupérés
+	 * @return bool false si rien n'est trouvé en base de donnée
+	 */
+	function get_AllCommentsByMembre(int $numMemb)
 	{
 		global $db;
 		$query = $db->prepare("SELECT * FROM comment WHERE numMemb=:numMemb");
@@ -47,7 +93,7 @@ class COMMENT
 		return $result;
 	}
 
-	function get_AllCommentsPlusByArticle($numArt)
+	function get_AllCommentsPlusByArticle(int $numArt)
 	{
 		global $db;
 		$query = $db->prepare("SELECT * FROM commentplus WHERE numArt=:numArt ORDER BY numSeqCom");
@@ -57,20 +103,28 @@ class COMMENT
 		$result = $query->fetchAll(PDO::FETCH_OBJ);
 		return $result;
 	}
-
-	/*function create($lib1Lang, $lib2Lang, $numPays)
+	
+	/**
+	 * create Permet d'ajouter un nouveau commentaire en base de donnée
+	 *
+	 * @param  mixed $numArt
+	 * @param  mixed $libCom
+	 * @param  mixed $numMemb
+	 * @return void
+	 */
+	function create(int $numArt, string $libCom, int $numMemb)
 	{
 		global $db;
-		require_once __DIR__ . '/getNextNumLang.php';
-		$numLang = getNextNumLang($numPays);
+		require_once __DIR__ . '/getNextNumCom.php';
+		$numSeqCom = getNextNumCom($numArt);
 		try {
 			$db->beginTransaction();
-			$query = $db->prepare('INSERT INTO langue (numLang, lib1Lang, lib2Lang, numPays) VALUES (:numLang, :lib1Lang, :lib2Lang, :numPays)');
+			$query = $db->prepare('INSERT INTO comment (numSeqCom, numArt, dtCreCom, libCom, attModOK, affComOK, numMemb) VALUES (:numSeqCom, :numArt, NOW(), :libCom, 1, 1, :numMemb)');
 			$query->execute([
-				'numLang' => $numLang,
-				'lib1Lang' => $lib1Lang,
-				'lib2Lang' => $lib2Lang,
-				'numPays' => $numPays
+				'numSeqCom' => $numSeqCom,
+				'numArt' => $numArt,
+				'libCom' => $libCom,
+				'numMemb' => $numMemb
 			]);
 			$db->commit();
 			$query->closeCursor();
@@ -80,17 +134,26 @@ class COMMENT
 			die('Erreur create COMMENT : ' . $e->getMessage());
 		}
 	}
-
-	function update($numLang, $lib1Lang, $lib2Lang)
+	
+	/**
+	 * update Permet de modifier le contenu d'un commentaire
+	 *
+	 * @param  mixed $numSeqCom
+	 * @param  mixed $numArt
+	 * @param  mixed $libCom
+	 * @param  mixed $numMemb
+	 * @return void
+	 */
+	function update(int $numSeqCom, int $numArt, string $libCom, int $numMemb)
 	{
 		global $db;
 		try {
 			$db->beginTransaction();
-			$query = $db->prepare('UPDATE langue SET lib1Lang=:lib1Lang, lib2Lang=:lib2Lang WHERE numLang=:numLang');
+			$query = $db->prepare('UPDATE comment SET libCom = :libCom WHERE numSeqCom=:numSeqCom AND numArt=:numArt');
 			$query->execute([
-				'numLang' => $numLang,
-				'lib1Lang' => $lib1Lang,
-				'lib2Lang' => $lib2Lang
+				'numSeqCom' => $numSeqCom,
+				'numArt' => $numArt,
+				'libCom' => $libCom
 			]);
 			$db->commit();
 			$query->closeCursor();
@@ -100,16 +163,51 @@ class COMMENT
 			die('Erreur update COMMENT : ' . $e->getMessage());
 		}
 	}
-
-	// Ctrl FK sur THEMATIQUE, ANGLE, MOTCLE avec del
-	function delete($numLang)
+	
+	/**
+	 * modDelete Permet à un modérateur de bannir un commentaire
+	 *
+	 * @param  mixed $numSeqCom
+	 * @param  mixed $numArt
+	 * @param  mixed $notifComKOAff
+	 * @return void
+	 */
+	function modDelete(int $numSeqCom, int $numArt, string $notifComKOAff)
 	{
 		global $db;
 		try {
 			$db->beginTransaction();
-			$query = $db->prepare('DELETE FROM langue WHERE numLang=:numLang');
+			$query = $db->prepare('UPDATE comment SET attModOK = 0, affComOK = 0, notifComKOAff = :notifComKOAff WHERE numSeqCom=:numSeqCom AND numArt=:numArt');
 			$query->execute([
-				'numLang' => $numLang
+				'numSeqCom' => $numSeqCom,
+				'numArt' => $numArt,
+				'notifComKOAff' => $notifComKOAff
+			]);
+			$db->commit();
+			$query->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$query->closeCursor();
+			die('Erreur mod delete COMMENT : ' . $e->getMessage());
+		}
+	}
+	
+	/**
+	 * delete Permet la suppression logique d'un commentaire
+	 *
+	 * @param  mixed $numSeqCom
+	 * @param  mixed $numArt
+	 * @return void
+	 */
+	function delete(int $numSeqCom, int $numArt) //Logical delete
+	{
+		global $db;
+		try {
+			$db->beginTransaction();
+			$query = $db->prepare('UPDATE comment SET delLogiq = 1 WHERE numSeqCom=:numSeqCom AND numArt=:numArt');
+			$query->execute([
+				'numSeqCom' => $numSeqCom,
+				'numArt' => $numArt,
 			]);
 			$db->commit();
 			$query->closeCursor();
@@ -119,5 +217,5 @@ class COMMENT
 			$query->closeCursor();
 			die('Erreur delete COMMENT : ' . $e->getMessage());
 		}
-	}*/
+	}
 }	// End of class
